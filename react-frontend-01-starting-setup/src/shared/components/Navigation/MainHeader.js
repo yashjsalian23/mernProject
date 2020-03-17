4719d7f832e9c8c1557886ddfe62e37638b017da
yashjsalian23 <yashjsalian23@gmail.com>
2020-03-17 19:27:23 +0700

    intial commit

--- react-frontend-01-starting-setup/src/shared/components/Navigation/MainHeader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './MainHeader.css';
-
-const mainHeader = props => {
-    return <div className="main-header">{props.children}</div>
-}
-
-export default mainHeader;
