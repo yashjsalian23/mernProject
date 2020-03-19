22fa3b8767a749cdd9c2454eba8ec27963d23754
yashjsalian23 <yashjsalian23@gmail.com>
2020-03-19 13:00:50 +0700

    intial commit

---
+++ node-backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const HttpError = require('./models/http-error');
+
+
+const placesRoutes = require('./routes/places-routes');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/api/places', placesRoutes); 
+
+app.use((req, res, next) => {
+  const error = new HttpError('Cannot find the Route', 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
+});
+
+app.listen(7000);
